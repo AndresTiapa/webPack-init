46d00456332bf8b99714a861cdfa5efda5ff2a70
Andres Tiapa <tiapa.andres@gmail.com>
2021-07-14 16:08:14 -0400

    docs creado

--- webpack.prod.js
+++ webpack.prod.js
@@ -78,17 +78,13 @@ module.exports = {
 
             },
             {
-                module: {
-                    rules: [{
-                        test: /\.m?js$/,
-                        exclude: /node_modules/,
-                        use: {
-                            loader: "babel-loader",
-                            options: {
-                                presets: ['@babel/preset-env']
-                            }
-                        }
-                    }]
+                test: /\.m?js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader",
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
                 }
             }
 
